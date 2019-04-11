395e7adaa5170c4101f9748112628c2f3a3881f8
jwarumze <jwarumze@JWARUMZE-MOBL>
2019-04-11 18:46:09 +0200

    Initial Commit

---
+++ flask_server.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+
+app = Flask(__name__,
+            static_url_path='/web/static')
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+if __name__ == "__main__":
+    app.run()
