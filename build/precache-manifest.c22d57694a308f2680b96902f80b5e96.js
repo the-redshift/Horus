45ec5dc1e4691f289bbfc11c1d21df2d31ace333
Dominika Hatała <39065822+DominikaHatala@users.noreply.github.com>
2019-05-14 21:10:53 +0200

    Set up react and bootstrap (#1)

---
+++ build/precache-manifest.c22d57694a308f2680b96902f80b5e96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad0c91b2f880f798bb02498dd89fa56e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaf8f16a52a62300ae17",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "d7457b0a81ddddd343c1",
+    "url": "/static/css/main.5cd671bc.chunk.css"
+  },
+  {
+    "revision": "eaf8f16a52a62300ae17",
+    "url": "/static/js/2.b0f4c8b7.chunk.js"
+  },
+  {
+    "revision": "d7457b0a81ddddd343c1",
+    "url": "/static/js/main.8d39ae04.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
