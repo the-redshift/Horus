bb8bdd0e637f6c8dc68a8e64a38a264db64103e2
dominika <doniahatala@gmail.com>
2019-05-16 22:24:29 +0200

    UI prototype

--- src/App.js
+++ src/App.js
@@ -1,42 +1,24 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import {
+    BrowserRouter as Router,
+    Route
+} from 'react-router-dom';
+import 'font-awesome/css/font-awesome.min.css';
 
-class App extends React.Component {
+import Login from './Login';
+import Main from './Main';
 
-render() {
-return (
-    <div class="app">
-
-	<div class="header">
-	  <h1 class="display-3">siemaneczko</h1>
-	  <p>taki tam poczateczek</p>
-	</div>
-
-      <div>
-
-        <div class="row">
-          <div class="col-md-3 column">
-            <h2>wybor kamery</h2>
-            <p>tutaj bedzie wybor kamer</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-          </div>
-          <div class="col-md-6 column">
-            <h2>kamery</h2>
-            <p> tutaj bedzie widok kamer</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-          </div>
-          <div class="col-md-3 column">
-            <h2>obiekty</h2>
-            <p> a tu proszem ja cb bedzie wybor obiektow</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-          </div>
-        </div>
-
-        <hr/>
-
-      </div> 
-
-    </div>
-);}}
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Route exact path="/" component={Login} />
+                    <Route path="/main" component={Main} />
+                </div>
+            </Router>
+        );
+    }
+}
 
 export default App;
---
+++ src/Login.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            password: '',
+            submitted: false,
+            loading: false,
+            error: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.setState({ submitted: true });
+        const { username, password } = this.state;
+        if(password === 'test' && username === 'test') {
+            let path = `/main`;
+            this.props.history.push(path);
+        } else {
+            this.setState({error: 'Invalid username or password'});
+        }
+    }
+
+    render() {
+        const { username, password, submitted, loading, error } = this.state;
+        return (
+            <div className="login">
+                <div className="col-md-6 offset-3">
+                    <h1 className="pt-3 pb-3">Login</h1>
+                    <form name="form" onSubmit={this.handleSubmit}>
+                        <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
+                            <label htmlFor="username">Username</label>
+                            <input type="text" className="form-control" name="username" value={username} onChange={this.handleChange} />
+                            {submitted && !username &&
+                            <div className="help-block">Username is required</div>
+                            }
+                        </div>
+                        <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
+                            <label htmlFor="password">Password</label>
+                            <input type="password" className="form-control" name="password" value={password} onChange={this.handleChange} />
+                            {submitted && !password &&
+                            <div className="help-block">Password is required</div>
+                            }
+                        </div>
+                        <div className="form-group">
+                            <button className="btn btn-secondary mt-3" disabled={loading}>Login</button>
+                            {loading}
+                        </div>
+                        {error &&
+                        <div className={'alert alert-danger'}>{error}</div>
+                        }
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;
