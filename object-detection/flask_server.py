71be57d4edf3156a3a9c3ac8524690cbb0ff9342
Apollo <jakubwarumzer@gmail.com>
2019-05-16 23:35:57 +0200

    Repo reorg (#7)

--- object-detection/flask_server.py
+++ object-detection/flask_server.py
@@ -1,10 +1,12 @@
 from flask import Flask, request
 
 app = Flask(__name__,
-            static_url_path='/web/static')
+            static_folder='C:/Users/jwarumze/Desktop/Horus/web/static')
+            # static_url_path='/web/static')
 
 @app.route('/')
 def root():
+    # return app.static_folder
     return app.send_static_file('index.html')
 
 if __name__ == "__main__":
