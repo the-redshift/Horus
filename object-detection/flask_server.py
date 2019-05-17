ca91bb1c4572cfdbdc0124f9f6f03f8e7c92f01d
jwarumze <jwarumze@JWARUMZE-MOBL>
2019-05-17 11:33:29 +0200

    Major fixes & improvements

--- object-detection/flask_server.py
+++ object-detection/flask_server.py
@@ -4,7 +4,8 @@ from flask import Flask, abort, jsonify, request
 from object_recognition import ObjectRecognition
 
 lock = Lock()
-recognized_objects = []
+recognized_objects = {}
+cameras = {}
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,14 +15,18 @@ def root():
 # Endpoint to start recognition on a given camera
 @app.route("/start/<camera_id>", methods=['POST'])
 def start_camera(camera_id):
+    filename = request.args.get('filename')
+    if filename == None:
+        abort(400)
+
     if camera_id in cameras:
         return "Camera already exists."
 
     path_to_labels = os.path.join('C:\\Users\\jwarumze\\TensorFlow\\models\\research\\object_detection\\data', \
              'mscoco_label_map.pbtxt')
-    path_to_vid = os.path.join('C:\\Users\\jwarumze\\Desktop\\', "car.avi")
+    path_to_vid = os.path.join('C:\\Users\\jwarumze\\Desktop\\', str(filename))
 
-    cameras[camera_id] = ObjectRecognition(50, path_to_vid, path_to_labels)
+    cameras[camera_id] = ObjectRecognition(camera_id, 0.50, path_to_vid, path_to_labels)
     cameras[camera_id].detect()
 
     resp = jsonify(success=True)
@@ -53,8 +58,9 @@ def specify_class_subset(camera_id):
 @app.route("/post-recognized-objects/<camera_id>", methods=['POST'])
 def post_objects(camera_id):
     try:
-        recognized = request.get_json(force=True)
-        recognized = recognized['recognized_objects']
+        recognized = request.args.get('recognized_objects')
+        if recognized == None:
+            abort(400)
 
         with lock:
             recognized_objects[camera_id] = recognized
